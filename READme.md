35203320f01fccbe9fcf56f72ace29234bc99bba
LdKhanh <khanhleemtp>
2020-09-14 15:25:59 +0700

    1-intro

---
+++ READme.md
@@ -0,0 +1,9 @@
+# 1
+- What is OOP? 
+ > OOP is coding
+ > Make our code more modular and reuseable
+ > Make our code easier
+ > Make it easier to debug when things go wrong
+
+# 2 
+- 
---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+    echo 'Hello php';
+?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP OOP</title>
+</head>
+<body>
+    
+</body>
+</html>
